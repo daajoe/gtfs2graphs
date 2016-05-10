039f079184d20a753e4f14525e56607585eb174e
daajoe <daajoe@ficht.es>
2016-05-10 14:51:40 +0200

    Added validation parameter

--- gtfs2graphs/run4allfeeds.sh
+++ gtfs2graphs/run4allfeeds.sh
@@ -3,6 +3,7 @@
 shpath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 options=':hc:'
+validate=""
 while getopts $options option
 do
     case $option in
@@ -12,6 +13,10 @@ do
             exit 2;;
     esac
 done
+
+if [ -z "$validate" ]; then
+    printf "No validation given.\n"
+fi
 
 shift $((OPTIND - 1))
 path=$1
@@ -23,9 +28,15 @@ fi
 
 for file in $(find $path/*.zip -maxdepth 1 -type f) ; do 
     printf "Running gtfs2graphs for %s.\n" $file
-    $shpath/gtfs2graphs.py $file
+    #$shpath/gtfs2graphs.py $file
     if [ $? -ne 0 ] ; then
 	printf "Error on %s.\n Exiting." %file
 	exit 2
     fi
+    outputfile=$path/gr/$(basename $file.gr)
+    if [ -n "$validate" ]; then
+	printf "Validate %s.\n Exiting." %outputfile
+	$validate/td-validate $outputfile
+    fi
+    exit 1
 done;
